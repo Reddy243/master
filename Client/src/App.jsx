db1d276d0513f611f9264f839139c1fce349edca
Reddy243 <87081562+Reddy243@users.noreply.github.com>
2025-05-17 22:48:22 +0530

    Update App.jsx
    Updated routers

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navigator from './components/Navigator/Navigator.jsx';
 import WelcomeMessage from './components/WelcomeMessage/WelcomeMessage.jsx';
@@ -13,50 +13,52 @@ import Shop from '/images/shop.svg';
 
 function App() {
   return (
-    <>
-      <header>
-        <div className="header-container">
-          <h1 id="mainTitle">Shop Name</h1>
-          <div className="shopLogo">
-            <img src={Shop} alt="ShopLogo" />
+    <Router>
+      <>
+        <header>
+          <div className="header-container">
+            <h1 id="mainTitle">Shop Name</h1>
+            <div className="shopLogo">
+              <img src={Shop} alt="ShopLogo" />
+            </div>
           </div>
-        </div>
-      </header>
-      
-      {/* Navigation Menu */}
-      <Navigator />
+        </header>
+        
+        {/* Navigation Menu */}
+        <Navigator />
 
-      {/* Main Content */}
-      <section>
-        <Routes>
-          {/* Home route (Default) */}
-          <Route path="/" element={
-            <>
-              <WelcomeMessage />
-              <Slideshow />
-            </>
-          } />
+        {/* Main Content */}
+        <section>
+          <Routes>
+            {/* Home route (Default) */}
+            <Route path="/" element={
+              <>
+                <WelcomeMessage />
+                <Slideshow />
+              </>
+            } />
 
-          {/* Order route */}
-          <Route path="/order" element={<Orderform />} />
+            {/* Order route */}
+            <Route path="/order" element={<Orderform />} />
 
-          {/* Address route */}
-          <Route path="/address" element={<Address />} />
+            {/* Address route */}
+            <Route path="/address" element={<Address />} />
 
-          {/* Contact route */}
-          <Route path="/contact" element={<Contact />} />
-          {/* Signup Route*/}
-          <Route path="/signup" element={<Signup />} />
-          {/* Feedback Route*/}
-          <Route path="/feedback" element={<Feedback />} />
-        </Routes>
-      </section>
+            {/* Contact route */}
+            <Route path="/contact" element={<Contact />} />
+            {/* Signup Route*/}
+            <Route path="/signup" element={<Signup />} />
+            {/* Feedback Route*/}
+            <Route path="/feedback" element={<Feedback />} />
+          </Routes>
+        </section>
 
-      {/* Footer */}
-      <footer>
-        <h5 id="footer">All rights reserved @2025</h5>
-      </footer>
-    </>
+        {/* Footer */}
+        <footer>
+          <h5 id="footer">All rights reserved @2025</h5>
+        </footer>
+      </>
+    </Router>
   );
 }
 
