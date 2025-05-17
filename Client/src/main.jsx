ae60558d5684155a3ac7696b8762305e42463e24
Reddy243 <87081562+Reddy243@users.noreply.github.com>
2025-05-17 23:00:07 +0530

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom'; // Add this line
-import { UserProvider } from './UserContext'; // Import UserProvider from UserContext
+import { HashRouter } from 'react-router-dom'; // ✅ Change to HashRouter
+import { UserProvider } from './UserContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> 
-      <UserProvider> {/* Wrap App with UserProvider to provide global context */}
+    <HashRouter> {/* ✅ HashRouter here */}
+      <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
